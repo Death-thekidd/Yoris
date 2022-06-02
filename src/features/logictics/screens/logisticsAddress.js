0db4b8d3021cb952e5c372f2b3eeb8f00576a409
karopeter <karopeter531@gmail.com>
2022-06-02 16:58:26 -0700

    Added new Logistics

---
+++ src/features/logictics/screens/logisticsAddress.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { LogisticContainer, LogisticPrimary } from '../components/address.styles';
+
+export default function LogisticAddress() {
+  return (
+    <LogisticContainer>
+      <LogisticPrimary>Save New Address?</LogisticPrimary>
+    </LogisticContainer>
+  );
+};
