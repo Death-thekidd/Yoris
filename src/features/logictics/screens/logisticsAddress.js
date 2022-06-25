ca1a42bc6ac11a534b5834a4392ce7daf120011f
karopeter <karopeter531@gmail.com>
2022-06-25 12:22:45 -0700

    deleted old designs

--- src/features/logictics/screens/logisticsAddress.js
+++ src/features/logictics/screens/logisticsAddress.js
@@ -14,33 +14,12 @@ export default function LogisticsAddress({ navigation }) {
       <LogisticsPrimary>Save New Address?</LogisticsPrimary>
       <LogisticsWrapper>
         <LogisticsRow>
-           <AddressBtn onPress={()=>navigation.navigate('logisticsGoods')}>Yes</AddressBtn>
+           <AddressBtn onPress={()=>navigation.navigate('shipping')}>Yes</AddressBtn>
         </LogisticsRow>
         <LogisticsRow>
             <AddressCover>No</AddressCover>
         </LogisticsRow>
       </LogisticsWrapper>
-      {/* <LogisticsWrapper>
-        <LogisticsRow>
-          <LogisticsSlot>Create new slot</LogisticsSlot>
-          <LogisticsBox></LogisticsBox>
-        </LogisticsRow>
-        <LogisticsRow>
-          <LogisticsReplace>Replace</LogisticsReplace>
-          <LogisticsBox></LogisticsBox>
-        </LogisticsRow>
-      </LogisticsWrapper>
-      <LogisticsWrapper>
-        <LogisticsRow>
-          <LogisticsReplace>Replace</LogisticsReplace>
-          <LogisticsBox></LogisticsBox>
-        </LogisticsRow>
-        <LogisticsRow>
-          <LogisticsReplace>Replace</LogisticsReplace>
-          <LogisticsBox></LogisticsBox>
-        </LogisticsRow>
-      </LogisticsWrapper>
-      <LogisticsBtn onPress={()=>navigation.navigate('logisticsGoods')}>continue</LogisticsBtn> */}
     </LogisticsContainer>
   );
 }
