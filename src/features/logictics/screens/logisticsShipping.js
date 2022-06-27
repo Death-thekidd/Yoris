8390dcd7a40b774031be508611070a44ee8dc647
ndukachukz <ndukachukwuemeka57@gmail.com>
2022-06-27 11:53:44 +0100

    fixed

--- src/features/logictics/screens/logisticsShipping.js
+++ src/features/logictics/screens/logisticsShipping.js
@@ -21,84 +21,72 @@ import {
 
 export default function LogisticsShipping({ navigation }) {
   return (
-        <Layout>
-          <ShippingWrapper>
-            <ShippingRow>
-              <Button onPress={() => navigation.navigate("logisticBooking")}>
-                <ImageBackground
-                  source={require("../../../../assets/backIcon.png")}
-                />
-              </Button>
-            </ShippingRow>
-            <ShippingRow>
-              <ShippingPrimary>Shipping Address</ShippingPrimary>
-            </ShippingRow>
-          </ShippingWrapper>
-          <ShippingParagraph>
-            Yoris<ShippingSpan>(Username)</ShippingSpan>
-          </ShippingParagraph>
-          <ShippingFeatures>
-            <ShippingAddress>Yoris Address</ShippingAddress>
-            <ImageShipping
-              source={require("../../../../assets/location.png")}
+    <Layout>
+      <ShippingWrapper>
+        <ShippingRow>
+          <Button onPress={() => navigation.navigate("logisticBooking")}>
+            <ImageBackground
+              source={require("../../../../assets/backIcon.png")}
             />
-          </ShippingFeatures>
-          <ShippingBoxInput
-            type="text"
-            placeholder={"Tracking Number From Shipping Company"}
-            placeholderTextColor={"#fff"}
-          />
-          <ShippingWrapper>
-            <ShippingRow>
-              <ShippingInput type="checkbox" id="navi-toggle">
-                <ShippingLabel>
-                  <ImageBitmap
-                    source={require("../../../../assets/group5.png")}
-                  />
-                </ShippingLabel>
-              </ShippingInput>
-            </ShippingRow>
-            <ShippingRow>
-              <ShippingInput type="checkbox" id="navi-toggle">
-                <ShippingLabel>
-                  <ImageBitmap
-                    source={require("../../../../assets/group1.png")}
-                  />
-                </ShippingLabel>
-              </ShippingInput>
-            </ShippingRow>
-            <ShippingRow>
-              <ShippingInput type="checkbox" id="navi-toggle">
-                <ShippingLabel>
-                  <ImageBitmap
-                    source={require("../../../../assets/group.png")}
-                  />
-                </ShippingLabel>
-              </ShippingInput>
-            </ShippingRow>
-            <ShippingRow>
-              <ShippingInput type="checkbox" id="navi-toggle">
-                <ShippingLabel>
-                  <ImageBitmap
-                    source={require("../../../../assets/group7.png")}
-                  />
-                </ShippingLabel>
-              </ShippingInput>
-            </ShippingRow>
-            <ShippingRow>
-              <ShippingInput type="checkbox" id="navi-toggle">
-                <ShippingLabel>
-                  <ImageBitmap
-                    source={require("../../../../assets/group3.png")}
-                  />
-                </ShippingLabel>
-              </ShippingInput>
-            </ShippingRow>
-          </ShippingWrapper>
-          <ShippingTracking>Add More Tracking Number</ShippingTracking>
-          <ShippingBtn onPress={() => navigation.navigate("logisticsVehicle")}>
-            continue
-          </ShippingBtn>
-        </Layout>
+          </Button>
+        </ShippingRow>
+        <ShippingRow>
+          <ShippingPrimary>Shipping Address</ShippingPrimary>
+        </ShippingRow>
+      </ShippingWrapper>
+      <ShippingParagraph>
+        Yoris<ShippingSpan>(Username)</ShippingSpan>
+      </ShippingParagraph>
+      <ShippingFeatures>
+        <ShippingAddress>Yoris Address</ShippingAddress>
+        <ImageShipping source={require("../../../../assets/location.png")} />
+      </ShippingFeatures>
+      <ShippingBoxInput
+        type="text"
+        placeholder={"Tracking Number From Shipping Company"}
+        placeholderTextColor={"#fff"}
+      />
+      <ShippingWrapper>
+        <ShippingRow>
+          <ShippingInput type="checkbox" id="navi-toggle">
+            <ShippingLabel>
+              <ImageBitmap source={require("../../../../assets/group5.png")} />
+            </ShippingLabel>
+          </ShippingInput>
+        </ShippingRow>
+        <ShippingRow>
+          <ShippingInput type="checkbox" id="navi-toggle">
+            <ShippingLabel>
+              <ImageBitmap source={require("../../../../assets/group1.png")} />
+            </ShippingLabel>
+          </ShippingInput>
+        </ShippingRow>
+        <ShippingRow>
+          <ShippingInput type="checkbox" id="navi-toggle">
+            <ShippingLabel>
+              <ImageBitmap source={require("../../../../assets/Group.png")} />
+            </ShippingLabel>
+          </ShippingInput>
+        </ShippingRow>
+        <ShippingRow>
+          <ShippingInput type="checkbox" id="navi-toggle">
+            <ShippingLabel>
+              <ImageBitmap source={require("../../../../assets/group7.png")} />
+            </ShippingLabel>
+          </ShippingInput>
+        </ShippingRow>
+        <ShippingRow>
+          <ShippingInput type="checkbox" id="navi-toggle">
+            <ShippingLabel>
+              <ImageBitmap source={require("../../../../assets/group3.png")} />
+            </ShippingLabel>
+          </ShippingInput>
+        </ShippingRow>
+      </ShippingWrapper>
+      <ShippingTracking>Add More Tracking Number</ShippingTracking>
+      <ShippingBtn onPress={() => navigation.navigate("logisticsVehicle")}>
+        continue
+      </ShippingBtn>
+    </Layout>
   );
 }
